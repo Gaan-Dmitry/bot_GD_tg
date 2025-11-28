b28a893cd54f11227fd2a61a750113cddc50dfe6
Gaan-Dmitry <Gaandima55@ya.ru>
2025-11-28 13:20:45 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# Конфигурация бота
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8501378717:AAGhzm-krzKpqBwxG_vB37dQvLkEeD_3cW8')
+ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID', '6297103998')
+
+# Конфигурация базы данных
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'u3299512_gaan-developments',
+    'user': 'u3299512_gaan-dmitry',
+    'password': 'yZU-gQW-cET-qVK',
+    'charset': 'utf8mb4',
+    'cursorclass': 'pymysql.cursors.DictCursor'
+}
